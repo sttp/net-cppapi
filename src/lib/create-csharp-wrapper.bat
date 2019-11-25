9e56ec518f910c01e0a484cafe8ae53c5a111efb
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-11-24 21:21:14 -0500

    Updated SWIG dllimport to remove ".dll" suffix from P/Invoke target so that needed suffix will be picked up based on platform, e.g., ".dll", ".so" and ".dylib"

--- src/lib/create-csharp-wrapper.bat
+++ src/lib/create-csharp-wrapper.bat
@@ -1,3 +1,3 @@
 @del /Q sttp.net\*.cs
-swig -c++ -csharp -outdir sttp.net -namespace sttp -dllimport sttp.net.lib.dll -o sttp.net.lib/sttp.net.lib.cpp sttp.i
+swig -c++ -csharp -outdir sttp.net -namespace sttp -dllimport sttp.net.lib -o sttp.net.lib/sttp.net.lib.cpp sttp.i
 @pause
