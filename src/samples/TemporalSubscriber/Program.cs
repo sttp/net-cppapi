9fd6424f7b8613ecfa25ab8ad4b8c6feb52f77ee
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2021-01-18 20:13:20 -0600

    Added a sample to show usage of a temporal subscription.

---
+++ src/samples/TemporalSubscriber/Program.cs
@@ -0,0 +1,121 @@
+﻿//******************************************************************************************************
+//  Program.cs - Gbtc
+//
+//  Copyright © 2019, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  01/18/2021 - J. Ritchie Carroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using Gemstone;
+
+namespace TemporalSubscriber
+{
+    internal class Program
+    {
+        // Set temporal parameters for historial read - note that the publisher is not
+        // obligated to honor requested parameters. Large historical windows or very
+        // fast replay rates may be refused.
+        private const string StartTime = "*-5m";    // Start historical replay starting 5-minutes ago (UTC)
+        private const string StopTime = "*";        // Stop historical replay at current time (UTC)
+        private const int ProcessInterval = 50;     // Set historical replay interval to 50 milliseconds
+
+        private static void Main(string[] args)
+        {
+            // Ensure that the necessary command line arguments are provided
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("    TemporalSubscriber HOSTNAME PORT");
+                return;
+            }
+
+            // Get hostname and port
+            string hostname = args[0];
+
+            if (!ushort.TryParse(args[1], out ushort port))
+                port = 7165;
+
+            long processStartTime = DateTime.UtcNow.Ticks;
+
+            SubscriberHandler subscriber = new SubscriberHandler
+            {
+                // Set the processing interval, in milliseconds, for the temporal subscription.
+                // With the exception of the values of -1 and 0, this value specifies the desired
+                // processing interval for data, i.e., basically a delay, or timer interval, over
+                // which to process data. A value of -1 means to use the default processing
+                // interval while a value of 0 means to process data as fast as possible.
+                ProcessInterval = ProcessInterval,
+
+                // Assign handler to run when historical read has completed
+                ReadCompleteHandler = (subscriptionCount, totalMeasurementsProcessed) =>
+                {
+                    Console.Write($"For time range \"{StartTime}\" to \"{StopTime}\":{Environment.NewLine}    ");
+
+                    if (totalMeasurementsProcessed == 0UL)
+                    {
+                        Console.WriteLine($"No historical data was read for {subscriptionCount:N0} subscribed points");
+                    }
+                    else
+                    {
+                        Ticks totalRuntime = DateTime.UtcNow.Ticks - processStartTime;
+                        Console.WriteLine($"Processed {totalMeasurementsProcessed:N0} measurements in {totalRuntime.ToElapsedTimeString(1)} ({totalMeasurementsProcessed / totalRuntime.ToSeconds():N0} measurements / second).");
+                    }
+                }
+            };
+
+            subscriber.Initialize(hostname, port);
+
+            // When the StartTime and StopTime temporal processing constraints are defined,
+            // the values specify the start and stop time over which the subscriber session
+            // desires data. When the parameters are defined, the subscriber is said to be
+            // requesting a temporal data session, i.e., requesting to replay history, this
+            // assuming the publisher stores history and supports temporal requests.
+            //
+            // Passing in null for the parameter values (the default) specifies that the
+            // subscriber session desires instead to process data in real-time mode, i.e.,
+            // subscription of published data as measured.
+            //
+            // The parameter values are string based so that the times can be set absolute
+            // or relative times. The following are example formats for the temporal
+            // processing start and stop time parameters:
+            //
+            //  Example:                  Description:
+            //  ------------------------  -------------------------------------------------
+            //  12-30-2000 23:59:59.033   specifies an absolute date and time
+            //            *               evaluates to current time (UTC)
+            //          *-20s             evaluates to 20 seconds before current time (UTC)
+            //          *-10m             evaluates to 10 minutes before current time (UTC)
+            //          *-1h              evaluates to 1 hour before current time (UTC)
+            //          *-1d              evaluates to 1 day before current time (UTC)
+            subscriber.EstablishHistoricalRead(StartTime, StopTime);
+
+            subscriber.FilterExpression = "FILTER TOP 10 ActiveMeasurements WHERE SignalType = 'FREQ' OR SignalType LIKE '%PH%'";
+
+            subscriber.ConnectAsync();
+
+            // Wait until the user presses a key before quitting.
+            Console.ReadKey();
+
+            // Shutdown subscriber instances
+            subscriber.Disconnect();
+
+            Console.WriteLine("Disconnected.");
+        }
+    }
+}
---
+++ src/samples/TemporalSubscriber/SubscriberHandler.cs
@@ -0,0 +1,165 @@
+﻿//******************************************************************************************************
+//  SubscriberHandler.cs - Gbtc
+//
+//  Copyright © 2019, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  01/18/2021 - J. Ritchie Carroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Text;
+using System.Threading;
+using sttp;
+
+namespace TemporalSubscriber
+{
+    public class SubscriberHandler : SubscriberInstance
+    {
+        private uint m_subscriptionCount;
+        private long m_lastMessageTime;
+        private static readonly object s_consoleLock = new object();
+
+        // Processing interval, in milliseconds, for the temporal subscription
+        public int ProcessInterval { get; init; } = -1;
+
+        // Call back for read complete - parameters are subscribed measurement count and total measurements processed
+        public Action<uint, ulong> ReadCompleteHandler { get; init; }
+
+        //protected override SubscriptionInfo CreateSubscriptionInfo()
+        //{
+        //    SubscriptionInfo info = base.CreateSubscriptionInfo();
+        //
+        //    // See SubscriptionInfo class in DataSubscriber.h for all properties:
+        //    // https://github.com/sttp/cppapi/blob/master/src/lib/transport/DataSubscriber.h#L41
+        //
+        //    // Examples:
+        //    //info.Throttled = false;
+        //    //info.IncludeTime = true;
+        //    //info.UseMillisecondResolution = true;
+        //
+        //    return info;
+        //}
+
+        protected override void SetupSubscriberConnector(SubscriberConnector connector)
+        {
+            base.SetupSubscriberConnector(connector);
+
+            // Enable auto-reconnect sequence:
+            connector.AutoReconnect = true;
+
+            // Set maximum number to attempt reconnection, -1 means never stop retrying connection attempts:
+            connector.MaxRetries = -1;
+
+            // Set number of initial milliseconds to wait before retrying connection attempt:
+            connector.RetryInterval = 1000;
+
+            // Set maximum number of milliseconds to wait before retrying connection attempt, connection
+            // retry attempts use exponential back-off algorithm up to this defined maximum:
+            connector.MaxRetryInterval = 6000;
+        }
+
+        protected override void StatusMessage(string message)
+        {
+            // TODO: Make sure these messages get logged to an appropriate location
+
+            // Calls can come from multiple threads, so we impose a simple lock before write to console
+            lock (s_consoleLock)
+                Console.WriteLine($"{message}\n");
+        }
+
+        protected override void ErrorMessage(string message)
+        {
+            // TODO: Make sure these messages get logged to an appropriate location
+
+            // Calls can come from multiple threads, so we impose a simple lock before write to console
+            lock (s_consoleLock)
+                Console.Error.WriteLine($"{message}\n");
+        }
+
+        // This reports timestamp of very first received measurement (if useful)
+        protected override void DataStartTime(DateTime startTime) => 
+            StatusMessage($"Received first measurement at timestamp {startTime:yyyy-MM-dd HH:mm:ss.fff}");
+
+        protected override void ReceivedMetadata(ByteBuffer payload)
+        {
+            StatusMessage($"Received {payload.Count:N0} bytes of metadata, parsing...");
+            base.ReceivedMetadata(payload);
+        }
+
+        protected override void ParsedMetadata() => 
+            StatusMessage("Metadata successfully parsed.");
+
+        public override void SubscriptionUpdated(SignalIndexCache signalIndexCache)
+        {
+            m_subscriptionCount = signalIndexCache.Count;
+            StatusMessage($"Publisher provided {m_subscriptionCount:N0} measurements in response to subscription.");
+
+            // Establish initial temporal processing interval as soon as subscription is ready. This value
+            // can be changed at anytime while historical replay is active.
+            SetHistoricalReplayInterval(ProcessInterval);
+        }
+
+        public override unsafe void ReceivedNewMeasurements(Measurement* measurements, int length)
+        {
+            long currentTime = DateTime.UtcNow.Ticks;
+            bool showMessage = new TimeSpan(currentTime - m_lastMessageTime).TotalSeconds > 5.0D;
+
+            if (showMessage)
+            {
+                m_lastMessageTime = currentTime;
+
+                StringBuilder message = new StringBuilder();
+
+                message.AppendLine($"{GetTotalMeasurementsReceived():N0} measurements received so far...");
+                message.AppendLine();
+
+                for (int i = 0; i < Math.Min(length, 10); i++)
+                {
+                    Measurement measurement = measurements[i];
+                    message.AppendLine($"({i + 1:D2}) [{measurement.GetSignalID()}] @ {measurement.GetDateTime():yyyy-MM-dd HH:mm:ss.fff}:");
+                    message.AppendLine($"\t{measurement.Value:N3}");
+                }
+
+                StatusMessage(message.ToString());
+            }
+        }
+
+        protected override void ConfigurationChanged() => 
+            StatusMessage("Configuration change detected. Metadata refresh requested.");
+
+        protected override void HistoricalReadComplete()
+        {
+            StatusMessage("Historical data read complete.");
+
+            if (ReadCompleteHandler is null)
+                return;
+
+            ulong totalMeasurementsProcessed = GetTotalMeasurementsReceived();
+
+            // Call read complete handler on separate thread so as to not
+            // hold up base class call for HistoricalReadComplete method
+            ThreadPool.QueueUserWorkItem(_ => ReadCompleteHandler(m_subscriptionCount, totalMeasurementsProcessed));
+        }
+
+        protected override void ConnectionEstablished() => 
+            StatusMessage("Connection established.");
+
+        protected override void ConnectionTerminated() => 
+            StatusMessage("Connection terminated.");
+    }
+}
